46297becd38c532d29c397fcc4a8c186479c3ee7
Karol Bargiel <karibargiel@gmail.com>
2025-10-06 20:42:43 +0200

    addiing app.properties for test

--- src/main/java/com/example/ExploRun/ExploRunApplication.java
+++ src/main/java/com/example/ExploRun/ExploRunApplication.java
@@ -2,7 +2,9 @@ package com.example.ExploRun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("ExploRun")
 @SpringBootApplication
 public class ExploRunApplication {
 
--- src/test/java/com/example/ExploRun/ExploRunApplicationTests.java
+++ src/test/java/com/example/ExploRun/ExploRunApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.ExploRun;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class ExploRunApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
