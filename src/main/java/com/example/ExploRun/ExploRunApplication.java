c441f5edcdbfd4b8af3b1e42f5ad01c71acd9e11
Karol Bargiel <karibargiel@gmail.com>
2025-10-06 10:19:29 +0200

    Project Init

---
+++ src/main/java/com/example/ExploRun/ExploRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExploRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExploRun/ExploRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExploRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
