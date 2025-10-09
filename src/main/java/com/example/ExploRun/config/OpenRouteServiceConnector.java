a0fad6b318e9f5ff2637f5937d8698a915900dca
Karol Bargiel <karibargiel@gmail.com>
2025-10-09 16:32:49 +0200

    trying to generate first routes

--- src/main/java/com/example/ExploRun/config/OpenRouteServiceConnector.java
+++ src/main/java/com/example/ExploRun/config/OpenRouteServiceConnector.java
@@ -3,12 +3,14 @@ package com.example.ExploRun.config;
 
 import com.example.ExploRun.dto.Point;
 import com.example.ExploRun.request.OpenRouteServiceRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class OpenRouteServiceConnector{
@@ -23,14 +25,39 @@ public class OpenRouteServiceConnector{
       @Value("${openrouteservice.api.url}") String baseUrl,
       @Value("${openrouteservice.api.key}") String apiKey)
   {
-    this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+    this.webClient = webClientBuilder.baseUrl(baseUrl + "/v2/directions").build();
     this.objectMapper = objectMapper;
     this.apiKey = apiKey;
   }
 
-/*  public String generateRoute(List<Point> waypoints, boolean isRound) {
-    OpenRouteServiceRequest openRouteServiceRequest = new OpenRouteServiceRequest();
+  public String generateRoute(List<Point> waypoints, boolean isRound) throws JsonProcessingException {
+    OpenRouteServiceRequest requestBodyObject;
 
+    if(isRound){
+      waypoints.add(waypoints.getFirst());
+      requestBodyObject = OpenRouteServiceRequest.builder()
+          .coordinates(waypoints)
+          .profile("foot-walking")
+          .preference("recommended")
+          .build();
+    }else{
+      requestBodyObject = OpenRouteServiceRequest.builder()
+          .coordinates(waypoints)
+          .profile("foot-walking")
+          .preference("recommended")
+          .build();
+    }
+    String requestBody = objectMapper.writeValueAsString(requestBodyObject);
 
-  }*/
+    String profilePath = requestBodyObject.getProfile();
+
+    return webClient.post()
+        .uri(uriBuilder -> uriBuilder.pathSegment(profilePath).build())
+        .header("Accept", "application/json, application/geo+json")
+        .header("Authorization", apiKey)
+        .bodyValue(requestBody)
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+  }
 }
--- src/main/java/com/example/ExploRun/request/OpenRouteServiceRequest.java
+++ src/main/java/com/example/ExploRun/request/OpenRouteServiceRequest.java
@@ -52,19 +52,4 @@ public class OpenRouteServiceRequest {
    */
   @JsonProperty("preference")
   private String preference;
-
-  /**
-   * Advanced options for routing
-   * avoid_features : ["highways", "tollways", "ferries"]
-   * avoid_countries : [...]
-   * avoid_polygons : [...]
-   * round_trip : [ {
-   *  length : 10000(meters),
-   *  points : 2 (integer),
-   *  seed : 1 (integer)
-   * } ]
-   */
-  @JsonProperty("options")
-  private Map<String, Object> options;
-
 }
